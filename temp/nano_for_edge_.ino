db8c779350e4e55c52b0f477750010f55c0e8f1f
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2022-12-14 18:51:38 +0500

    nano_for_second_patient_data

--- temp/nano_for_edge_.ino
+++ temp/nano_for_edge_.ino
@@ -1,16 +1,33 @@
-void setup() { Serial.begin(115200); }
-void loop() {
-  Serial.print("SKdRiOAIL4NMiAkgrfmq@");
 
+// in this code nano will genarate data for two patients
+#include <SoftwareSerial.h>
+
+SoftwareSerial patient_2(2, 3);
+void setup() {
+  Serial.begin(115200);
+  patient_2.begin(115200);
+}
+void loop() {
+
+  Serial.print("SKdRiOAIL4NMiAkgrfmq@");
+  patient_2.print("3a2v3Vlb5UD6RFx0Eyg3");
   // temperature
   Serial.print(random(35, 37));
   Serial.print(".");
   Serial.print(random(1, 1000));
   Serial.print("!");
 
+  patient_2.print(random(35, 37));
+  patient_2.print(".");
+  patient_2.print(random(1, 1000));
+  patient_2.print("!");
+
   // position
   Serial.print("spine_position");
   Serial.print("!");
+
+  patient_2.print("Spine_position");
+  patient_2.print("!");
 
   // GSR
   Serial.print(random(1, 3));
@@ -30,11 +47,35 @@ void loop() {
   Serial.print(random(1, 100));
   Serial.print("!");
 
+  patient_2.print(random(1, 3));
+  patient_2.print(".");
+  patient_2.print(random(1, 1000));
+
+  patient_2.print(",");
+
+  patient_2.print(random(100000, 999999));
+  patient_2.print(".");
+  patient_2.print(random(1, 100));
+
+  patient_2.print(",");
+
+  patient_2.print(random(0, 3));
+  patient_2.print(".");
+  patient_2.print(random(1, 100));
+  patient_2.print("!");
+
   // snore
   Serial.print(random(1, 3));
   Serial.print(".");
   Serial.print(random(1, 100));
   Serial.print("!");
   Serial.println("#");
+
+  patient_2.print(random(1, 3));
+  patient_2.print(".");
+  patient_2.print(random(1, 100));
+  patient_2.print("!");
+  patient_2.println("#");
+
   delay(2000);
 }
